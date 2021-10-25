91f6fac382008f6bdec03c38d397812d9c6179f7
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2021-10-25 03:25:37 -0300

    install bootstrap

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const fetch = require("node-fetch");
 
 async function inicialize() {
     await db.init();
-
+    
+        
+    
     const http = require("http");
     const app = require("./src/app.js");
     const server = http.createServer(app);
-
-
+    
+    
+    
     server.listen(process.env.PORT || 5000, () => {
         console.log("|-----------------------------------------------------------------------------------------------------------------|");
         console.log("|- BANCO DE DADOS ONLINE!" + " HORARIO DE INICIALIZAÇÃO " + new Date() + "-|");
--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 const routerManager = require("./router/routerManager.js");
-
+const bootstrap_path = require("path");
 
 const checkAuth = require("./auth/checkAuth");
+
+
 app.use("/api/v1", checkAuth);
 
 app.use("/api/v1", routerManager);
 
 app.use("/", async(req, res, next) => {
+    res.render("api-mysql/layouts/index.js");
+    
     res.status(404).json({
         message: "not found"
     });
 });
+
+app.use(express.static(bootstrap_path.join(__dirname, "public")));
+
+
 module.exports = app;
