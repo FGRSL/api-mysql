34a7a86f69241bcbc028753ca4ff27dd4cd7e099
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2021-10-25 02:53:43 -0300

    Create get.js

---
+++ src/router/routes/browser/get.js
@@ -0,0 +1,9 @@
+﻿const router = require("express").Router();
+const db = require("../../../database.js");
+
+
+router.get("/about", async( req, res, next) =>{
+    res.send("site teste!!!");
+})
+
+module.exports = router;
